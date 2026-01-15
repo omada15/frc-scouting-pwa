2b155d1c57a512a5311af55ae2e1bb164f43cb92
Max Fal <fox4evergame@gmail.com>
2026-01-14 19:34:14 -0500

    adds a quick way to get to stored data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="fixed bottom-0 left-0 w-full bg-zinc-800 text-gray-300 py-2 text-center">
             <p>Created by members of Team 3464 "Sim-City".</p>
+            <a href="./stored">Stored Data </a>
         </footer>
     );
 };
