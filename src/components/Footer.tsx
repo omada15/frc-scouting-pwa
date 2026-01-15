92418d09ee6d182ba145888a2789cc7070a67076
Max Fal <fox4evergame@gmail.com>
2026-01-14 20:15:14 -0500

    my foot is kind of shoeless

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="fixed bottom-0 left-0 w-full bg-zinc-800 text-gray-300 py-2 text-center">
+        <footer className="bottom-0 left-0 w-full bg-zinc-800 text-gray-300 py-2 text-center">
             <p>Created by members of Team 3464 "Sim-City".</p>
             <a href="./stored" className="text-center text-gray-300">Stored Data </a>
         </footer>
