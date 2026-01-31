807775b3e22c3a2f241e2959fa5b846e18cf1027
sam <omada916@gmail.com>
2026-01-30 19:53:28 -0500

    mango

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+const router = express.Router();
+
+router.post('/write', (req, res) => {
+  res.send('Hello, World!');
+  console.log(req.body);
+});
