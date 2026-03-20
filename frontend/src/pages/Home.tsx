f14c2b01b90017a79d898b06505cc025701b569b
Daniel <dan.senchukov@gmail.com>
2026-03-20 12:26:49 -0400

    redo test changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -38,17 +38,16 @@ const Home: React.FC = () => {
         }
     }, []);
     
-    //
-    //if (readCookie("user") == "Daniel Senchukov") {
-        //setNameCSS("font-bold text-yellow-200 text-4xl underline")
-   // } else {
-       // setNameCSS("font-bold text-white text-4xl underline")
-   // }
-    //--}
+    
+    if (readCookie("user") == "Daniel Senchukov") {
+        setNameCSS("font-bold text-yellow-200 text-4xl underline")
+    } else {
+        setNameCSS("font-bold text-white text-4xl underline")
+    }
 
     return (
         <div className="flex flex-col items-center justify-center space-y-6">
-            <h1 className="font-bold text-white text-4xl underline">
+            <h1 className={nameCSS}>
                 {readCookie("user") === undefined
                     ? "Welcome to Sim-scouting!"
                     : "Welcome to Sim-scouting, " + readCookie("user")}
